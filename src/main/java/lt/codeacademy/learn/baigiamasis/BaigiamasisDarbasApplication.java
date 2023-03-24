ebf9adc8068a439a2429ff3fb4905d548bc35ea0
rokas058 <rokas.prabusas@gmail.com>
2023-03-24 10:54:22 +0200

    Spring pirmas ikelimas

---
+++ src/main/java/lt/codeacademy/learn/baigiamasis/BaigiamasisDarbasApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.learn.baigiamasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamasisDarbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaigiamasisDarbasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/codeacademy/learn/baigiamasis/BaigiamasisDarbasApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.learn.baigiamasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaigiamasisDarbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
