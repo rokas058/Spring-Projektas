4917f822164b71f041806952423de3602f8515c2
rokas058 <rokas.prabusas@gmail.com>
2023-04-14 10:34:03 +0300

    user dummy add

--- src/main/java/lt/codeacademy/learn/baigiamasis/admin/CreateAdmin.java
+++ src/main/java/lt/codeacademy/learn/baigiamasis/admin/CreateAdmin.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Component;
 
 import lt.codeacademy.learn.baigiamasis.user.User;
 import lt.codeacademy.learn.baigiamasis.user.UserRepository;
-import static lt.codeacademy.learn.baigiamasis.user.Roles.ADMIN;;
+import static lt.codeacademy.learn.baigiamasis.user.Roles.ADMIN;
+import static lt.codeacademy.learn.baigiamasis.user.Roles.USER;;
 
 
 @Component
@@ -30,8 +31,39 @@ public class CreateAdmin implements CommandLineRunner{
 			user.setPassword( encoder.encode("admin") );
 			user.setRole(ADMIN);
 			user.setEnable(true);
-			user = userRepository.save(user);
+			userRepository.save(user);
 		}
+		User user1 = new User();
+		user1.setEmail("a.b@gmail.com");
+		user1.setfirstName("a");
+		user1.setLastName("b");
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		user1.setPassword( encoder.encode("1234"));
+		user1.setRole(USER);
+		user1.setEnable(true);
+		userRepository.save(user1);
+
+		User user2 = new User();
+		user2.setEmail("a.c@gmail.com");
+		user2.setfirstName("a");
+		user2.setLastName("c");
+		PasswordEncoder encoder2 = new BCryptPasswordEncoder();
+		user2.setPassword( encoder.encode("1234"));
+		user2.setRole(USER);
+		user2.setEnable(true);
+		userRepository.save(user2);
+
+		User user3 = new User();
+		user3.setEmail("a.d@gmail.com");
+		user3.setfirstName("a");
+		user3.setLastName("d");
+		PasswordEncoder encoder3 = new BCryptPasswordEncoder();
+		user3.setPassword( encoder.encode("1234"));
+		user3.setRole(USER);
+		user3.setEnable(true);
+		userRepository.save(user3);
+
+
 			
 		
 		
