387bbf1dbf1e9bc91c2a5cd2e3aa9d88f82e9aed
rokas058 <rokas.prabusas@gmail.com>
2023-04-17 08:42:45 +0300

    pridetas Corsfilter

--- src/main/java/lt/codeacademy/learn/baigiamasis/security/WebSecurityConfig.java
+++ src/main/java/lt/codeacademy/learn/baigiamasis/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package lt.codeacademy.learn.baigiamasis.security;
 
 
 import lt.codeacademy.learn.baigiamasis.user.Roles;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,7 +20,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.AllArgsConstructor;
 import lt.codeacademy.learn.baigiamasis.user.UserService;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +31,8 @@ public class WebSecurityConfig{
 
 	private JwtAuthEntryPoint authEntryPoint;
 	private UserService userService;
+
+
 	
 	@Bean
 	public SecurityFilterChain filtherChain(HttpSecurity http) throws Exception {
@@ -45,6 +51,7 @@ public class WebSecurityConfig{
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
+			.addFilterBefore(corsFilter(), ChannelProcessingFilter.class)
 			.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 			.httpBasic();
 
@@ -76,6 +83,18 @@ public class WebSecurityConfig{
 	@Bean
 	public JwtAuthenticationFilter jwtAuthenticationFilter(){
 		return new JwtAuthenticationFilter();
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
 
